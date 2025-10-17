4ffcb0233513a4e703dad16cd2dd661f6885cd70
PauloAugusto <paulo.augustog@outlook.com>
2025-10-17 17:38:56 -0400

    primeiro commit - estrtutura inicial do projeto

---
+++ src/test/java/org/example/DesenvolvedorTest.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DesenvolvedorTest {
+
+    @org.junit.jupiter.api.Test
+    void getNivelBaseadoEmExperiencia() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void adicionarProjeto() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removerProjeto() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void calcularHorasPorProjeto() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getProjetos() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNome() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setNome() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNivel() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setNivel() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAnosDeExperiencia() {
+    }
+}
